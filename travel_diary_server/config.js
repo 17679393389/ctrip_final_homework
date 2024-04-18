26a6714d4e4faf1ca9c5013c81ff99857b6004b0
Shanlong Qiu <qsl163email@163.com>
2024-04-18 19:37:07 +0800

    添加启动页面

--- travel_diary_server/config.js
+++ travel_diary_server/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   secretKey: "65sd45f6as4dg4sa3d5g4s54sad65g4",
-  appid: "wx82317ad92bd318ba",
-  appsecret: "c786c522bce74154f4c886ab0337222f",
+  appid: "wx2f2c7c617b0ae5f8",
+  appsecret: "75225a773388e4e7563b087b30ee56d7",
 };
